6ab7d885516946cb59b025fdf067a593f529e984
Dan Lee <danielhlee4@gmail.com>
2023-08-26 12:27:35 -0400

    add #add and #addBlocks to Game class

--- src/game.js
+++ src/game.js
@@ -1,10 +1,16 @@
+import { forEach } from "core-js/core/array";
 import Block from "./block.js";
 
 class Game {
     constructor(ctx) {
         this.ctx;
-        this.blocks = this.createMatrix(5);
+        this.blocks = this.createMatrix(Game.NUM_COLS);
+
+        this.addBlocks();
     }
+
+    static NUM_COLS = 5;
+    static NUM_HEIGHT = 5; // how many blocks high a column can be
 
     createMatrix(cols) {
         const matrix = [];
@@ -13,5 +19,17 @@ class Game {
             matrix.push(col);
         }
         return matrix;
+    }
+
+    add(col) {
+        this.blocks[col].push(new Block({game: this}))
+    }
+
+    addBlocks() {
+        this.blocks.forEach((col) => {
+            for (let i = 0; i < Game.NUM_HEIGHT; i++) {
+                this.add(i);
+            }
+        })
     }
 }
