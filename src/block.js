140b6792ea6ede9e4220f1c5bd5886583cf223d0
Dan Lee <danielhlee4@gmail.com>
2023-08-26 14:47:12 -0400

    add #getRandomColor

--- src/block.js
+++ src/block.js
@@ -2,16 +2,22 @@ class Block {
     constructor(options) {
         this.x = options.x;
         this.y = options.y;
-        this.dimensions = Block.DIMS;
-        this.color = options.color;
         this.ctx = options.ctx
+        this.color = Block.getRandomColor(Block.COLORS);
+        this.dims = Block.DIMS;
     }
 
     static DIMS = 50;
+    static COLORS = ["#FF0000", "#00FF00", "#0000FF"];
+
+    getRandomColor(arr) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        return arr[randomIndex];
+    }
 
     draw(ctx) {
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.dimensions, this.dimensions);
+        ctx.fillRect(this.x * this.dims, this.y * this.dims, this.dims, this.dims);
     }
 }
 
